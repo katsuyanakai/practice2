52604443e65f7b388b132fe2af4225e1bb3d18e8
katsuyanakai <katsuyanakai88@gmail.com>
2015-05-01 23:12:21 +0900

    3rd commit

--- practice/practice/ViewController.swift
+++ practice/practice/ViewController.swift
@@ -45,6 +45,45 @@ class ViewController: UIViewController {
         // ViewにLabelを追加.
         self.view.addSubview(myLabel)
 
+
+        
+        let button = UIButton()
+        
+        //表示されるテキスト
+        button.setTitle("Tap Me!", forState: .Normal)
+        
+        //テキストの色
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        
+        //タップした状態のテキスト
+        button.setTitle("Tapped!", forState: .Highlighted)
+        
+        //タップした状態の色
+        button.setTitleColor(UIColor.redColor(), forState: .Highlighted)
+        
+        //サイズ
+        button.frame = CGRectMake(0, 0, 300, 50)
+        
+        //配置場所
+        button.layer.position = CGPoint(x: self.view.frame.width/2, y:100)
+        
+        //背景色
+        button.backgroundColor = UIColor(red: 0.7, green: 0.2, blue: 0.2, alpha: 0.2)
+        
+        //角丸
+        button.layer.cornerRadius = 10
+        
+        //ボーダー幅
+        button.layer.borderWidth = 1
+        
+        //ボタンをタップした時に実行するメソッドを指定
+        //button.addTarget(self, action: "tapped:", forControlEvents:.TouchUpInside)
+        
+        //viewにボタンを追加する
+        self.view.addSubview(button)
+        
+
+    
     }
 
     override func didReceiveMemoryWarning() {
